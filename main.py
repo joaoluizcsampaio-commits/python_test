c6cd2dfcd5f7723774845849ed3535567a285d6d
Joao Sampaio <joaoluizcsampaio@gmail.com>
2026-01-21 12:02:40 +0000

    First commit

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-def in_auto_tests_we_trust(a, b):
+def in_autotests_we_trust(a, b):
     if a == b:
         print('Passou no teste')
     else:
         print('Falhou no teste')
-in_auto_tests_we_trust(10, '10')
 
-in_auto_tests_we_trust(0, False)
+in_autotests_we_trust(10, '10')
+
+in_autotests_we_trust(0, False)
